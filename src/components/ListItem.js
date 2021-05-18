2a836fce8515925a18182ad1ef823bc5d06b1c22
ndkv9 <vu.nguyen101@gmail.com>
2021-05-18 21:15:52 +0300

    change Link to anchor

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 
 const ListItem = ({ location, item }) => {
@@ -11,7 +10,7 @@ const ListItem = ({ location, item }) => {
 	)
 }
 
-const ListItemLink = styled(Link)`
+const ListItemLink = styled.a`
 	display: inline-block;
 	padding: 0.2rem 0;
 	width: 100%;
