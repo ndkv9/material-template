9eee41b7ecce8cc73d063dc537f9cb506bf4ae48
ndkv9 <vu.nguyen101@gmail.com>
2021-05-25 14:27:09 +0300

    modify page title with Helmet

--- src/templates/docs.js
+++ src/templates/docs.js
@@ -4,49 +4,51 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 import React from 'react'
 import Layout from '../components/Layout'
 import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 import { MDXProvider } from '@mdx-js/react'
 import { Exercise, Note } from '../components/UIComponents'
 import { Image, AspectImage, Embed } from 'theme-ui'
 
 const DocsTemplate = ({ data, location }) => {
-	const { mdx } = data
+  const { mdx } = data
 
-	const shortcodes = { Image, AspectImage, Embed, Exercise, Note }
+  const shortcodes = { Image, AspectImage, Embed, Exercise, Note }
 
-	return (
-		<Layout tableOfContents={mdx.tableOfContents} location={location}>
-			<Heading>{mdx.frontmatter.title}</Heading>
-			<MDXProvider components={shortcodes}>
-				<MDXRenderer>{mdx.body}</MDXRenderer>
-			</MDXProvider>
-		</Layout>
-	)
+  return (
+    <Layout tableOfContents={mdx.tableOfContents} location={location}>
+      <Helmet title={mdx.frontmatter.title} />
+      <Heading>{mdx.frontmatter.title}</Heading>
+      <MDXProvider components={shortcodes}>
+        <MDXRenderer>{mdx.body}</MDXRenderer>
+      </MDXProvider>
+    </Layout>
+  )
 }
 
 const Heading = styled.h1`
-	padding-top: 0;
-	margin-top: 0;
-	&::before {
-		display: none !important;
-	}
+  padding-top: 0;
+  margin-top: 0;
+  &::before {
+    display: none !important;
+  }
 `
 
 export const pageQuery = graphql`
-	query($id: String!) {
-		mdx(fields: { id: { eq: $id } }) {
-			body
-			tableOfContents
-			frontmatter {
-				title
-			}
-		}
-	}
+  query($id: String!) {
+    mdx(fields: { id: { eq: $id } }) {
+      body
+      tableOfContents
+      frontmatter {
+        title
+      }
+    }
+  }
 `
 DocsTemplate.propTypes = {
-	data: PropTypes.shape({
-		mdx: PropTypes.object.isRequired,
-	}).isRequired,
-	location: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    mdx: PropTypes.object.isRequired,
+  }).isRequired,
+  location: PropTypes.object.isRequired,
 }
 
 export default DocsTemplate
