fca330579a3db5ec19adfd3384ca62317b0e854f
ndkv9 <vu.nguyen101@gmail.com>
2021-06-08 16:37:18 +0300

    adjust sidebar open subpages when get clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,38 +5,39 @@ import IconButton from './icons/IconButton'
 import Menu from './icons/Menu'
 import Close from './icons/Close'
 import LogoWrapper from './LogoWrapper'
+import PropTypes from 'prop-types'
 
 const Header = ({ navOpen, setNavOpen }) => {
-	return (
-		<StyledHeader navOpen={navOpen}>
-			<HeaderSection>
-				<NavIconButton>
-					<IconButton
-						label='Open Navigation'
-						icon={navOpen ? <Close /> : <Menu />}
-						size={30}
-						onClick={() => {
-							setNavOpen(!navOpen)
-						}}
-					/>
-				</NavIconButton>
-				<LogoWrapper />
-			</HeaderSection>
-		</StyledHeader>
-	)
+  return (
+    <StyledHeader navOpen={navOpen}>
+      <HeaderSection>
+        <NavIconButton>
+          <IconButton
+            label='Open Navigation'
+            icon={navOpen ? <Close /> : <Menu />}
+            size={30}
+            onClick={() => {
+              setNavOpen(!navOpen)
+            }}
+          />
+        </NavIconButton>
+        <LogoWrapper />
+      </HeaderSection>
+    </StyledHeader>
+  )
 }
 
 const StyledHeader = styled.header`
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	padding: 1rem 1rem;
-	z-index: 5;
-	background: ${p => p.theme.colors.header};
-	transition: all 0.25s var(--ease-in-out-quad);
-	border-bottom: 1px solid ${p => p.theme.colors.borderColor};
-	transform: ${p => (p.navOpen ? 'translateX(19rem)' : null)};
-	${mediaqueries.desktop_up`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 1rem 1rem;
+  z-index: 5;
+  background: ${p => p.theme.colors.header};
+  transition: all 0.25s var(--ease-in-out-quad);
+  border-bottom: 1px solid ${p => p.theme.colors.borderColor};
+  transform: ${p => (p.navOpen ? 'translateX(19rem)' : null)};
+  ${mediaqueries.desktop_up`
     position: fixed;
     top: 0;
     left: 0;
@@ -48,18 +49,23 @@ const StyledHeader = styled.header`
 `
 
 const NavIconButton = styled.div`
-	display: flex;
-	margin-right: 1rem;
-	${mediaqueries.desktop_up`
+  display: flex;
+  margin-right: 1rem;
+  ${mediaqueries.desktop_up`
     display: none;
   `};
 `
 
 const HeaderSection = styled.div`
-	display: flex;
-	margin: 0;
-	text-alight: center;
-	align-items: center;
+  display: flex;
+  margin: 0;
+  text-alight: center;
+  align-items: center;
 `
+
+Header.propTypes = {
+  navOpen: PropTypes.bool,
+  setNavOpen: PropTypes.func,
+}
 
 export default Header
--- src/components/LeftSidebar/Navigation.js
+++ src/components/LeftSidebar/Navigation.js
@@ -5,41 +5,41 @@ import React, { useState } from 'react'
 import NavItem from './NavItem'
 
 const Navigation = () => {
-	const result = useStaticQuery(graphql`
-		query {
-			allMdx(
-				filter: { frontmatter: { hidden: { ne: true } } }
-				sort: { fields: frontmatter___nav_order, order: ASC }
-			) {
-				edges {
-					node {
-						fields {
-							title
-							slug
-						}
-						tableOfContents
-					}
-				}
-			}
-		}
-	`)
+  const result = useStaticQuery(graphql`
+    query {
+      allMdx(
+        filter: { frontmatter: { hidden: { ne: true } } }
+        sort: { fields: frontmatter___nav_order, order: ASC }
+      ) {
+        edges {
+          node {
+            fields {
+              title
+              slug
+            }
+            tableOfContents
+          }
+        }
+      }
+    }
+  `)
 
-	const { allMdx } = result
-	const [treeData] = useState(() => {
-		return calculateTreeData(allMdx.edges)
-	})
+  const { allMdx } = result
+  const [treeData] = useState(() => {
+    return calculateTreeData(allMdx.edges)
+  })
 
-	return (
-		<NavList>
-			<NavItem {...treeData} />
-		</NavList>
-	)
+  return (
+    <NavList>
+      <NavItem {...treeData} />
+    </NavList>
+  )
 }
 
 const NavList = styled.ul`
-	margin: 0;
-	padding: 0;
-	list-style: none;
+  margin: 0;
+  padding: 0;
+  list-style: none;
 `
 
 export default React.memo(Navigation)
