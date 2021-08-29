1e8fd35fad5a0cba93bd22f6f3992dc764aa60fb
ndkv9 <vu.nguyen101@gmail.com>
2021-08-29 14:31:33 +0300

    adjust file styling

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,82 +9,82 @@ const startCase = require('lodash/startCase')
 
 // access markdown files with a GraphQL data query
 exports.createPages = async ({ actions, graphql, reporter }) => {
-	const result = await graphql(
-		`
-			{
-				allMdx(
-					filter: { frontmatter: { hidden: { ne: true } } }
-					sort: { fields: frontmatter___nav_order, order: ASC }
-				) {
-					edges {
-						node {
-							fields {
-								id
-								slug
-							}
-							tableOfContents
-						}
-					}
-				}
-			}
-		`,
-	)
-	if (result.errors) {
-		reporter.panic('error loading content', result.errors)
-		return
-	}
-	// creates content for template files
-	result.data.allMdx.edges.forEach(({ node }) => {
-		actions.createPage({
-			path: node.fields.slug ? node.fields.slug : '/',
-			component: require.resolve('./src/templates/docs'),
-			context: {
-				id: node.fields.id,
-			},
-		})
-	})
+  const result = await graphql(
+    `
+      {
+        allMdx(
+          filter: { frontmatter: { hidden: { ne: true } } }
+          sort: { fields: frontmatter___nav_order, order: ASC }
+        ) {
+          edges {
+            node {
+              fields {
+                id
+                slug
+              }
+              tableOfContents
+            }
+          }
+        }
+      }
+    `,
+  )
+  if (result.errors) {
+    reporter.panic('error loading content', result.errors)
+    return
+  }
+  // creates content for template files
+  result.data.allMdx.edges.forEach(({ node }) => {
+    actions.createPage({
+      path: node.fields.slug ? node.fields.slug : '/',
+      component: require.resolve('./src/templates/docs'),
+      context: {
+        id: node.fields.id,
+      },
+    })
+  })
 }
 
 exports.onCreateNode = ({ node, getNode, actions, reporter }) => {
-	const { createNodeField } = actions
+  const { createNodeField } = actions
 
-	if (node.internal.type === 'Mdx') {
-		const parent = getNode(node.parent)
-		const title = node.frontmatter.title || startCase(parent.name)
+  if (node.internal.type === 'Mdx') {
+    const parent = getNode(node.parent)
+    const title = node.frontmatter.title || startCase(parent.name)
 
-		let value = node.frontmatter.slug
-		if (!value && parent.relativePath) {
-			value = parent.relativePath.replace(parent.ext, '')
-		}
+    let value = node.frontmatter.slug
+    if (!value && parent.relativePath) {
+      value = parent.relativePath.replace(parent.ext, '')
+    }
 
-		if (!value) {
-			reporter.panic(
-				`Can not create node with title: ${title} there is no relative path or frontmatter to set the "slug" field`,
-			)
-			return
-		}
+    if (!value) {
+      reporter.panic(
+        `Can not create node with title: ${title} there is no relative path or frontmatter to set the "slug" field`,
+      )
+      return
+    }
 
-		if (value === 'index') {
-			value = ''
-		}
+    if (value === 'index') {
+      value = ''
+    }
 
-		// the file name is the slug itself
-		createNodeField({
-			name: 'slug',
-			node,
-			value: `/${value}`,
-		})
+    // the file name is the slug itself
+    createNodeField({
+      name: 'slug',
+      node,
+      value: `/${value}`,
+    })
 
-		createNodeField({
-			name: 'id',
-			node,
-			value: node.id,
-		})
+    createNodeField({
+      name: 'id',
+      node,
+      value: node.id,
+    })
 
-		createNodeField({
-			name: 'title',
-			node,
-			value: title,
-		})
-	}
+    createNodeField({
+      name: 'title',
+      node,
+      value: title,
+    })
+  }
 }
